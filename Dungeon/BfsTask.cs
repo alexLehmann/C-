fba7040c76baf79e7f4096db79051649ffa53157
Aleksej <Lehmann.aleksej@gmail.com>
2019-12-09 23:09:44 +0700

    Graph  bfs
    Нужно проверить на больших картах

---
+++ Dungeon/BfsTask.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Dungeon
+{
+    public class BfsTask
+    {
+        public static bool IsPointVisited(HashSet<SinglyLinkedList<Point>> hs, SinglyLinkedList<Point> p) =>
+                    hs.Where(x => x.Value.X == p.Value.X && x.Value.Y == p.Value.Y).Any();
+
+        public static IEnumerable<SinglyLinkedList<Point>> FindPaths(Map map, Point start, Point[] chests)
+        {
+            HashSet<SinglyLinkedList<Point>> visited = new HashSet<SinglyLinkedList<Point>>();
+            Queue<SinglyLinkedList<Point>> queue = new Queue<SinglyLinkedList<Point>>();
+            queue.Enqueue(new SinglyLinkedList<Point>(start, null));
+           
+            while (queue.Count != 0)
+            {
+                SinglyLinkedList<Point> point = queue.Dequeue();
+                if (point.Value.X < 0 || point.Value.X >= map.Dungeon.GetLength(0)
+                    || point.Value.Y < 0 || point.Value.Y >= map.Dungeon.GetLength(1))
+                    continue;
+                if (map.Dungeon[point.Value.X, point.Value.Y] != MapCell.Empty || IsPointVisited(visited, point))
+                    continue;
+                visited.Add(point);
+                if (chests.Contains(point.Value))
+                {
+                    yield return point;
+                }
+                for (var dy = -1; dy <= 1; dy++)
+                    for (var dx = -1; dx <= 1; dx++)
+                        if (dx != 0 && dy != 0) continue;
+                        else queue.Enqueue(new SinglyLinkedList<Point>(new Point(point.Value.X + dx, point.Value.Y + dy), point));
+            }
+            yield break;
+        }
+    }
+}
---
+++ Dungeon/DungeonTask.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon
+{
+	public class DungeonTask
+	{        
+        private static List<List<Point>> PathCollectionToListPath(IEnumerable<SinglyLinkedList<Point>> path, bool flagReverse)
+        {
+            List<List<Point>> list = new List<List<Point>>();
+            foreach (var item in path)
+            {
+                list.Add(item.ToList());
+                if (flagReverse) list[list.Count - 1].Reverse();
+            }
+            return list;
+        }
+
+        private static List<Point> SearchShortesPath(List<List<Point>> list)
+        {
+            int minCount = Int32.MaxValue;
+            int indexList = 0;
+            for (int i = 0; i < list.Count; i++)
+            {               
+                if (minCount > list[i].Count)
+                {
+                    minCount = list[i].Count;
+                    indexList = i;
+                }
+            }
+            return list[indexList].ToList();
+        }
+
+        private static IEnumerable<List<Point>> JoinStartPathAndEndPath(List<List<Point>> pathStart, List<List<Point>> pathEnd) =>
+            pathStart.Join(pathEnd,
+                x => x.Last(),
+                y => y.First(),
+                (x, y) => x.Concat(y.Skip(1)).ToList());
+
+        private static IEnumerable<MoveDirection> PathConvertToMoveDirection(List<Point> path)=>
+            path.Zip(path.Skip(1),
+                      (a, b) => Walker.ConvertOffsetToDirection(new Size(b) - new Size(a)));
+
+
+        public static MoveDirection[] FindShortestPath(Map map)
+		{            
+            var pathStart = BfsTask.FindPaths(map, map.InitialPosition, map.Chests);
+            var pathEnd = BfsTask.FindPaths(map, map.Exit, map.Chests);
+            var listStart = PathCollectionToListPath(pathStart,true);
+            var listEnd = PathCollectionToListPath(pathEnd,false);
+            var result = JoinStartPathAndEndPath(listStart,listEnd);
+            if (result.Count() == 0)
+            {
+                var path = BfsTask.FindPaths(map, map.InitialPosition, new Point[] { map.Exit });
+                result = PathCollectionToListPath(path, true).ToList();                 
+            }
+            if (result.Count() == 0) return new MoveDirection[0];
+            List<Point> shortesPath = SearchShortesPath(result.ToList());
+            var pathMoveDirection = PathConvertToMoveDirection(shortesPath);                     
+            return pathMoveDirection.ToArray();
+        }
+	}
+}
